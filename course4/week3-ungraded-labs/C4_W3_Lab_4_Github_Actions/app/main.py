754fb12ce835de7a2e295258fa4d6b03ee749109
Mateus Oliveira <mtsodf@gmail.com>
2024-04-09 15:03:12 -0300

    Comment to activate github trigger

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-
+# Comment to activate github trigger
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
